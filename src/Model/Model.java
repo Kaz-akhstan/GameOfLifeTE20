61c910c32e865d84000e1cf3377110199dc86a66
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-12-16 13:38:40 +0100

    Skapat world, konverterar Arraylist till Array

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,16 +1,28 @@
 package Model;
 import View.*;
 
-public class Model {
-    public Model(int width, int height) {
+import java.util.ArrayList;
 
+public class Model {
+    int width, height;
+    private World world;
+    public Model(int width, int height) {
+        this.width = width;
+        this.height = height;
+        world = new World(width, height);
     }
 
     public void update() {
+        Shape[] cells = getShapes();
     }
 
     public Shape[] getShapes() {
-        Point[] points = {new Point(5,5)};
-        return (Shape[])points;
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(new Point(5,5));
+        points.add(new Point(6,5));
+        points.add(new Point(7,5));
+        Point[] p = new Point[points.size()];
+        points.toArray(p);
+        return (Shape[])p;
     }
 }
---
+++ src/Model/World.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class World {
+    int width, height;
+    public World(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
